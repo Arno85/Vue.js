979909add6fdcfb2d59bc98576fd4b795acd24fd
Arno85 <arnaud.martin85@gmail.com>
2021-02-02 18:05:29 -0500

    vue first app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Vue.js
+
+## Installation
+
+### CLI
+
+run `npm i -g @vue/cli`;
+
+## Create project
+
+run `vue create appName`
---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :friend="friend"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "1",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "2",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
